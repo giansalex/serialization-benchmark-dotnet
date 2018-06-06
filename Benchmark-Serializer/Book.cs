2274fa65176c07282fe09337dcabe66711f493af
Giancarlos Salas <giansalex@gmail.com>
2018-06-06 09:25:14 -0500

    add book and serializer bench

---
+++ Benchmark-Serializer/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Benchmark_Serializer
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ Benchmark-Serializer/Serializers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+
+namespace Benchmark_Serializer
+{
+    public class Serializers
+    {
+        private const int Max = 100;
+
+        public Serializers()
+        {
+            
+        }
+
+        public IEnumerable<Book> GetList()
+        {
+            var fixture = new Fixture();
+
+            foreach (var _ in Enumerable.Range(1, Max))
+            {
+                yield return fixture.Create<Book>();
+            }
+        }
+    }
+}
